9b099f31e7facc4b10e2e5b33acbc2f1d0f10dc7
tushar7139 <115531846+tushar7139@users.noreply.github.com>
2025-11-04 23:28:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (79.18%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> pre(nums), suf(nums), ans(size(nums));
-        partial_sum(begin(pre), end(pre), begin(pre), multiplies<int>());       // calculates & stores prefix product at each index
-        partial_sum(rbegin(suf), rend(suf), rbegin(suf), multiplies<int>());    // calculates & stores suffix product at each index
-        for(int i = 0; i < size(nums); i++)
-            ans[i] = (i ? pre[i-1] : 1) * (i+1 < size(nums) ? suf[i+1] : 1);
-        return ans;
+        int prod = 1, zeroCnt = count(begin(nums), end(nums), 0);
+        if(zeroCnt > 1) return vector<int>(size(nums));               // Case-1
+        for(auto c : nums) 
+            if(c) prod *= c;                                          // calculate product of all elements except 0
+        for(auto& c : nums)
+            if(zeroCnt) c = c ? 0 : prod;                             // Case-2
+            else c = prod / c;                                        // Case-3
+        return nums;
     }
 };
